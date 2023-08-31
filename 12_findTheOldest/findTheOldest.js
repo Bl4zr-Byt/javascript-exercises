cd7b33ffc197a23e5cee9222c949f4a2ac360732
Bl4zr-Byt <139316911+Bl4zr-Byt@users.noreply.github.com>
2023-09-01 00:34:06 +0530

    All foundations js exercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function(peopleArr) {
+const findTheOldest = function (peoplesArr) {
+  let date = new Date().getFullYear();
   
+  let oldest = peoplesArr.reduce((initial, current) => {
+
+    if (!("yearOfDeath" in initial)) {
+      initial.yearOfDeath = date;
+    }
+
+    currentAge = current.yearOfDeath - current.yearOfBirth;
+    initialAge = initial.yearOfDeath - initial.yearOfBirth;
+
+    if (initialAge > currentAge) return initial;
+    else return current
+
+  }, peoplesArr[0])
+
+  return oldest;
 };
 
 // Do not edit below this line
